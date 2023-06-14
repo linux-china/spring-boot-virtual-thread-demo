0b3ea1f545438fa425dfe2b95eb6bb948157c102
Libing Chen <libing.chen@gmail.com>
2023-06-14 17:13:42 +0800

    app: TomcatVirtualThreadConfiguration

---
+++ src/main/java/org/mvnsearch/boot/TomcatVirtualThreadConfiguration.java
@@ -0,0 +1,45 @@
+package org.mvnsearch.boot;
+
+import org.apache.catalina.core.StandardVirtualThreadExecutor;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+@Configuration
+public class TomcatVirtualThreadConfiguration {
+    /**
+     * Tomcat StandardVirtualThreadExecutor bean
+     *
+     * @return standard virtual thread executor
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public StandardVirtualThreadExecutor standardVirtualThreadExecutor() {
+        return new StandardVirtualThreadExecutor();
+    }
+
+    /**
+     * AsyncTaskExecutor to enable async servlet support
+     *
+     * @param standardVirtualThreadExecutor standard virtual thread executor
+     * @return async task executor
+     */
+    @Bean
+    AsyncTaskExecutor applicationTaskExecutor(StandardVirtualThreadExecutor standardVirtualThreadExecutor) {
+        return new TaskExecutorAdapter(standardVirtualThreadExecutor);
+    }
+
+    /**
+     * tomcat protocol handler customizer to enable virtual thread executor
+     *
+     * @param standardVirtualThreadExecutor standard virtual thread executor
+     * @return TomcatProtocolHandlerCustomizer
+     */
+    @Bean
+    TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer(StandardVirtualThreadExecutor standardVirtualThreadExecutor) {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(standardVirtualThreadExecutor);
+        };
+    }
+}
