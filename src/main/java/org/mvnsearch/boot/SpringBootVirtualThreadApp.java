85e8e351366de03b1a2fcc513b442b12361ae082
Libing Chen <libing.chen@gmail.com>
2023-06-14 17:13:27 +0800

    app(boot): SpringBootVirtualThreadApp

---
+++ src/main/java/org/mvnsearch/boot/SpringBootVirtualThreadApp.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVirtualThreadApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVirtualThreadApp.class, args);
+    }
+
+}
