bce4aea5a9c59df27af61d08d750a7ca4e6a653a
Libing Chen <libing.chen@gmail.com>
2023-06-15 10:37:19 +0800

    feat: add @Async support

--- src/main/java/org/mvnsearch/boot/PortalController.java
+++ src/main/java/org/mvnsearch/boot/PortalController.java
@@ -1,5 +1,6 @@
 package org.mvnsearch.boot;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class PortalController {
 
     @GetMapping("/")
     public String index() {
+        sayHi("Jackie");
         return "Hello Virtual Thread!";
     }
 
@@ -22,5 +24,10 @@ public class PortalController {
     @GetMapping("/where-am-i-async")
     Callable<String> getAsyncThreadName() {
         return () -> Thread.currentThread().toString();
+    }
+
+    @Async
+    public void sayHi(String name) {
+        System.out.println("Hi " + name + ", on " + Thread.currentThread());
     }
 }
