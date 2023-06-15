1ee28c29ea0db0b02952ed7867af7e0f6e73d881
Libing Chen <libing.chen@gmail.com>
2023-06-15 10:36:30 +0800

    feat: add @EnableAsync

--- src/main/java/org/mvnsearch/boot/SpringBootVirtualThreadApp.java
+++ src/main/java/org/mvnsearch/boot/SpringBootVirtualThreadApp.java
@@ -2,8 +2,10 @@ package org.mvnsearch.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringBootVirtualThreadApp {
 
     public static void main(String[] args) {
