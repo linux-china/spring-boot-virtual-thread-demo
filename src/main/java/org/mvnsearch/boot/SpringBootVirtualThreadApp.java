d3fe8bc8660a8e5778d3ff758466b7e395986a84
Libing Chen <libing.chen@gmail.com>
2023-06-23 18:50:00 +0800

    chore: polishing

--- src/main/java/org/mvnsearch/boot/SpringBootVirtualThreadApp.java
+++ src/main/java/org/mvnsearch/boot/SpringBootVirtualThreadApp.java
@@ -2,10 +2,8 @@ package org.mvnsearch.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class SpringBootVirtualThreadApp {
 
     public static void main(String[] args) {
